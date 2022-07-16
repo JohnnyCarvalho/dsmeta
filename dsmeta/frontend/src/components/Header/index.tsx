075678ad6c647d446da076e9674f5d288bf1c008
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-07-16 14:43:31 -0300

    Commit other componnents

--- dsmeta/frontend/src/components/Header/index.tsx
+++ dsmeta/frontend/src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import logo from '../../assets/img/logo.svg';
+import './styles.css';
 
 export function Header () {
   return (
@@ -6,10 +7,6 @@ export function Header () {
         <div className="dsmeta-logo-container">
             <img src={logo} alt="DSMeta" />
             <h1>DSMeta</h1>
-            <p>
-              Desenvolvido por
-              <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
-            </p>
         </div>
     </header>
   );
