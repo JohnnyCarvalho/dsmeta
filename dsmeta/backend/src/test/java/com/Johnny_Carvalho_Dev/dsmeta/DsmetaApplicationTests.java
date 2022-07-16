dcf7d8e0b3b36d5a6f3d2b16143d38d8e13aeb1a
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-07-16 09:56:08 -0300

    backend
    Create project spring.

---
+++ dsmeta/backend/src/test/java/com/Johnny_Carvalho_Dev/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Johnny_Carvalho_Dev.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
